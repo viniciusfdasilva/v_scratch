75d433fc65fc37cf160bac11d3a1831c6b90cb5f
Vinicius Silva <me@silvavinicius.com.br>
2023-12-25 11:32:39 -0300

    For iterator implemented on for.v

--- 06_repetitions/for.v
+++ 06_repetitions/for.v
@@ -28,5 +28,48 @@ pub fn main() {
 
 	// ==================================== //
 
+	nums := [1,2,3,4,5]
+	iter := SquareIterator {
+		arr : nums
+	}
+
+	for squared in iter {
+		println(squared)
+	}
+
+	// ==================================== //
+
+
 	
 }
+
+
+struct SquareIterator {
+	arr []int
+
+
+mut:
+	idx int
+}
+
+fn (mut iter SquareIterator) next() ?int{
+	if iter.idx >= iter.arr.len {
+		return none
+	}else {
+		defer {
+			iter.idx++
+		}
+		return iter.arr[iter.idx]
+	}
+}
+
+//fn (mut iter SquareIterator) next() ?int {
+//	if iter.idx >= iter.arr.len {
+//		return none
+//	}
+//	defer {
+//		iter.idx++
+//	}
+//
+//	return iter.arr[iter.idx] * iter.arr[iter.idx]
+//}
