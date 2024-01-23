b800b3c07bf3bb086c85bd6eeab15b108997bf55
Vinicius Silva <me@silvavinicius.com.br>
2024-01-22 22:23:27 -0300

    File interface created

---
+++ 16_interfaces/interfaces.v
@@ -0,0 +1,26 @@
+module main
+
+interface Foo{
+	write(string) string
+}
+
+interface Bar{
+mut:
+	write(string) string
+}
+
+struct MyStruct{}
+
+fn (s MyStruct) write(a string) string{
+	return a
+}
+
+fn main(){
+	s1 := MyStruct{}
+
+	fn1(s1)
+}
+
+fn fn1(s Foo){
+	println(s.write('Foo'))
+}
